cda86dc70a1288de36bc9fb3583f0197e8f0aae9
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-12-12 14:31:01 +0000

    developed contact section / used app.post on /contact/send-message endpoint

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, 'uploads/'); // Katalog, do którego będą zapisywane pliki
+	},
+	filename: (req, file, cb) => {
+		cb(null, file.originalname); // Oryginalna nazwa pliku
+	},
+});
+const upload = multer({ storage: storage });
 
 const app = express();
 
@@ -8,6 +19,7 @@ app.engine('hbs', hbs());
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
 	res.render('index');
@@ -32,6 +44,20 @@ app.get('/history', (req, res) => {
 app.get('/hello/:name', (req, res) => {
 	res.render('hello', { layout: false, name: req.params.name });
 });
+
+app.post(
+	'/contact/send-message',
+	upload.single('projectDesign'),
+	(req, res) => {
+		const { author, sender, title, message } = req.body;
+
+		if (author && sender && title && message && req.file) {
+			res.render('contact', { isSent: true, file: req.file.originalname });
+		} else {
+			res.render('contact', { isError: true });
+		}
+	}
+);
 
 app.use((req, res) => {
 	res.status(404).send('404 not found...');
